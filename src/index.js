2eced840b55d2b2348ff39e0fcf81c103af56181
jaballogian <jaballogianrma77@gmail.com>
2020-10-26 14:12:45 +0700

    added react-service-worker dependency to the project and changed default ui of react initialization

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "antd/dist/antd.css";
+// import * as serviceWorker from "./serviceWorker";
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
@@ -15,3 +16,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// serviceWorker.unregister();
